0d3c5d74eda0d98fe4b6ac8b1d29af70ed797edf
William Silveira <williamsilveiraf@hotmail.com>
2023-10-30 07:22:18 -0300

    fix: env config

--- src/contexts/AudioAnalysis/http.ts
+++ src/contexts/AudioAnalysis/http.ts
@@ -3,8 +3,8 @@ import { UploadResponse, AudioAnalysisState } from "./types";
 export async function postUploadFile(file: File): Promise<UploadResponse> {
     const formData = new FormData();
     formData.append('file', file);
-
-    const response = await fetch('http://localhost:8000/upload/', {
+    
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/upload/`, {
         method: 'POST',
         body: formData,
     });
@@ -13,7 +13,7 @@ export async function postUploadFile(file: File): Promise<UploadResponse> {
 }
 
 export async function getAudioAnalysis(id: number): Promise<AudioAnalysisState> {
-    const response = await fetch(`http://localhost:8000/audio/${id}`)
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/audio/${id}`)
     
     return response.json()
 }
